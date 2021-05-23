9c508666b29742eb02cbb26ea7d19c8ddcc248ea
eliassonbi <46024842+eliassonbi@users.noreply.github.com>
2021-05-23 13:21:42 +0200

    Update Main.java

--- src/main/java/fr/ul/miage/dsw/projetgl/Main.java
+++ src/main/java/fr/ul/miage/dsw/projetgl/Main.java
@@ -1,5 +1,16 @@
 package fr.ul.miage.dsw.projetgl;
 
 public class Main {
+  
+   try ( Scanner scanner = new Scanner( System.in ) ) {
+     
+     while(true){
+        System.out.print( "Bonjour ! Que souhaitez vous faire ?" );
+        String  line = scanner.nextLine();
+        Commandes cmd = new Commandes();
+        cmd.parser("connexion -numServeur 3 -table 1");
+     }   
+            
+     }
 
 }
